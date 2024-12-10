e30d68767cc23751e98d7458853f4940dbac59f9
MYOUSUFFS <mailyousuffsend@gmail.com>
2024-12-10 12:28:21 +0530

    index fle add

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import express from "express";
 import connetion from "./src/config/connection.js";
-import { fetchAllData, purchaseData, finduserinv } from "./src/collection.js";
+import {
+  fetchAllData,
+  purchaseData,
+  finduserinv,
+  getUserRec,
+} from "./src/invoice/collection.js";
+import {
+  stockval,
+  getTotpay,
+  getTotReci,
+  getTodayRept,
+  getTodayPaymt,
+  getTotTaxAmt,
+} from "./src/stock/collection.js";
 const app = express();
 app.use(express.json());
+app.get("/todayPaymt", getTodayPaymt);
+app.get("/todayRecipt", getTodayRept);
+app.get("/reciable", getTotReci);
+app.get("/panding", getTotpay);
+app.get("/stockval", stockval);
+app.get("/getUserRec", getUserRec);
 app.get("/userinv", finduserinv);
 app.get("/api/purchase", purchaseData);
 app.get("/sales", fetchAllData);
+app.get("/getax", getTotTaxAmt);
 connetion();
 app.listen(4000, () => {
   console.log("server is running on port 4000");
