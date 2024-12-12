a62da9f32c64d8d9fe34f7c5d0c649394cb4d8b2
MYOUSUFFS <mailyousuffsend@gmail.com>
2024-12-12 17:24:46 +0530

    function router add

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ import {
   getTodayRept,
   getTodayPaymt,
   getTotTaxAmt,
+  getdashboard,
 } from "./src/stock/collection.js";
+import getTaxReport from "./src/report/collection.js";
+
 const app = express();
 app.use(express.json());
 app.get("/todayPaymt", getTodayPaymt);
@@ -25,7 +28,9 @@ app.get("/getUserRec", getUserRec);
 app.get("/userinv", finduserinv);
 app.get("/api/purchase", purchaseData);
 app.get("/sales", fetchAllData);
-app.get("/getax", getTotTaxAmt);
+app.get("/getdashboard", getdashboard);
+app.get("/getax", getTaxReport);
+
 connetion();
 app.listen(4000, () => {
   console.log("server is running on port 4000");
